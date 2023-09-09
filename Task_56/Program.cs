c1a2014a08c1cf8593733ac74c11d302138f6b4f
Aliyor Azizovich <aliyor.0276@gmail.com>
2023-09-09 16:44:40 +0500

    task_56

---
+++ Task_56/Program.cs
@@ -0,0 +1,25 @@
+﻿static int FindRowWithMinSum(int[,] array)
+{
+    int minSum = int.MaxValue;
+    int minRow = -1;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (sum < minSum)
+        {
+            minSum = sum;
+            minRow = i+1;
+        }
+    }
+
+    return minRow;
+}
+// проверка
+int[,] array = new int[4, 4] { {1, 4, 7, 2}, {5, 9, 2, 3}, {8, 4, 2, 4}, {5, 2, 6, 7} };
+int minRow = FindRowWithMinSum(array);
+Console.WriteLine("Row with minimum sum of elements is: " + minRow);
