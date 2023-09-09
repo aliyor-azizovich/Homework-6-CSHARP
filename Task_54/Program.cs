001387c81227fa1404d66c7c4c2fbcbbb3a8cb39
Aliyor Azizovich <aliyor.0276@gmail.com>
2023-09-09 16:37:58 +0500

    task_54

--- Task_54/Program.cs
+++ Task_54/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void SortDescending(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, j] < array[i, k])
+                {
+                    int temp = array[i, j];
+                    array[i, j] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+// проверка
+int[,] array = new int[3, 4] { {1, 4, 7, 2}, {5, 9, 2, 3}, {8, 4, 2, -104} };
+SortDescending(array);
